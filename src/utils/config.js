d0897160cf96e28ba786777c9ba894cd66790e7b
weitaoYan6515 <45583036+weitaoYan6515@users.noreply.github.com>
2021-03-29 13:42:41 +0800

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,14 +1,14 @@
 export default {
     requestConfig:{
-        url:'http://121.5.11.127/tanke/',
+        url:'',
         Timout:5000
     },
     cosConfig:{
-        url: 'https://tanke-1304758273.cos.ap-shanghai.myqcloud.com/',
-        Bucket: 'tanke-1304758273',
-        Region: 'ap-shanghai',
-        SecretId: 'AKIDOPKDuPEOeoJhZHOvVs61m4C9TaYPmjLv',
-        SecretKey: 'bF3sRM281a2GUt584gFhArKa9jItGPGI'
+        url: '',
+        Bucket: '',
+        Region: '',
+        SecretId: '',
+        SecretKey: ''
     }
 }
     
